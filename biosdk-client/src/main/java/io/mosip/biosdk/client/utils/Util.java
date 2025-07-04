d0ad701d8b88be7d5f8897ce6b9eca6038001d6c
trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
2025-07-04 11:33:03 +0530

    MOSIP-42083-Performance Improvement - biosdk-client
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- biosdk-client/src/main/java/io/mosip/biosdk/client/utils/Util.java
+++ biosdk-client/src/main/java/io/mosip/biosdk/client/utils/Util.java
@@ -1,5 +1,30 @@
 package io.mosip.biosdk.client.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.mosip.biosdk.client.config.LoggerConfig;
+import io.mosip.kernel.core.exception.ExceptionUtils;
+import io.mosip.kernel.core.logger.spi.Logger;
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.util.Base64;
+import java.util.Map;
+
 import static io.mosip.biosdk.client.constant.AppConstants.LOGGER_IDTYPE;
 import static io.mosip.biosdk.client.constant.AppConstants.LOGGER_SESSIONID;
 
@@ -30,52 +55,32 @@ import io.mosip.kernel.core.logger.spi.Logger;
  */
 public class Util {
 
-	private static final GsonHttpMessageConverter MESSAGE_CONVERTER;
+	private static RestTemplate REST_TEMPLATE = null;
 
-	private static final RestTemplate REST_TEMPLATE;
+	private static final String debugRequestResponse = System.getenv("mosip_biosdk_request_response_debug");
+    private static final String MAX_CONN_PER_ROUTE = "mosip_biosdk_max_conn_per_host";
+    private static final String MAX_TOT_CONN = "mosip_biosdk_max_total_conn";
+    private static final String SSL_BYPASS = "mosip_biosdk_ssl_bypass";
+    private static boolean sslBypass = true;
+    private static Logger utilLogger = LoggerConfig.logConfig(Util.class);
 
-	static {
-		MESSAGE_CONVERTER = new GsonHttpMessageConverter();
-		REST_TEMPLATE = new RestTemplate();
-		REST_TEMPLATE.getMessageConverters().add(MESSAGE_CONVERTER);
-	}
+    public static ResponseEntity<?> restRequest(String url, HttpMethod httpMethodType, MediaType mediaType, Object body,
+                                             Map<String, String> headersMap, Class<?> responseClass) {
+        ResponseEntity<?> response = null;
 
-	private static Logger utilLogger = LoggerConfig.logConfig(Util.class);
-
-	private Util() {
-	}
-
-	/**
-	 * Makes a RESTful API request using the specified HTTP method, media type,
-	 * request body, headers, and expects a response of the specified class type.
-	 *
-	 * @param url            The URL of the RESTful API endpoint.
-	 * @param httpMethodType The HTTP method type (GET, POST, PUT, DELETE, etc.).
-	 * @param mediaType      The media type of the request body.
-	 * @param body           The request body object.
-	 * @param headersMap     Map containing headers to be included in the request.
-	 * @param responseClass  The class type of the expected response.
-	 * @return ResponseEntity containing the response body and HTTP status.
-	 * @throws RestClientException If the REST request fails.
-	 */
-	@SuppressWarnings({ "java:S1452" })
-	public static ResponseEntity<?> restRequest(String url, HttpMethod httpMethodType, MediaType mediaType, Object body,
-			Map<String, String> headersMap, Class<?> responseClass) {
-		ResponseEntity<?> response = null;
-		RestTemplate restTemplate = getRestTemplate();
-
-		try {
-			HttpHeaders headers = new HttpHeaders();
-			headers.setContentType(mediaType);
-			HttpEntity<?> request = null;
-			if (headersMap != null) {
-				headersMap.forEach((k, v) -> headers.add(k, v));
-			}
-			if (body != null) {
-				request = new HttpEntity<>(body, headers);
-			} else {
-				request = new HttpEntity<>(headers);
-			}
+        try {
+            RestTemplate restTemplate = getRestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(mediaType);
+            HttpEntity<?> request = null;
+            if (headersMap != null) {
+                headersMap.forEach((k, v) -> headers.add(k, v));
+            }
+            if (body != null) {
+                request = new HttpEntity<>(body, headers);
+            } else {
+                request = new HttpEntity<>(headers);
+            }
 
 			if (getDebugRequestResponse() != null && getDebugRequestResponse().equalsIgnoreCase("y")) {
 				Gson gson = new GsonBuilder().serializeNulls().disableHtmlEscaping().create();
@@ -83,37 +88,84 @@ public class Util {
 			}
 			response = restTemplate.exchange(url, httpMethodType, request, responseClass);
 
-			Object responseBodyObject = response.getBody();
-			String responseBody = responseBodyObject != null ? responseBodyObject.toString() : "";
-
-			if (getDebugRequestResponse() != null && getDebugRequestResponse().equalsIgnoreCase("y")) {
-				utilLogger.debug(LOGGER_SESSIONID, LOGGER_IDTYPE, "Response: ", responseBody);
-			}
-		} catch (RestClientException ex) {
-			utilLogger.error(LOGGER_SESSIONID, LOGGER_IDTYPE, "errors", ex);
-			throw new RestClientException("rest call failed");
-		}
-		return response;
+            if(debugRequestResponse != null && debugRequestResponse.equalsIgnoreCase("y")){
+                utilLogger.debug(LOGGER_SESSIONID, LOGGER_IDTYPE, "Response: ", response.getBody().toString());
+            }
+        } catch (RestClientException ex) {
+            ex.printStackTrace();
+            throw new RestClientException("rest call failed" + ExceptionUtils.getStackTrace(ex));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            throw new RestClientException("rest call failed" + ExceptionUtils.getStackTrace(e));
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+            throw new RestClientException("rest call failed" + ExceptionUtils.getStackTrace(e));
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+            throw new RestClientException("rest call failed" + ExceptionUtils.getStackTrace(e));
+        }
+        return response;
 	}
 
-	/**
-	 * Retrieves the configured REST template instance for making HTTP requests.
-	 *
-	 * @return Configured RestTemplate instance.
-	 */
-	private static RestTemplate getRestTemplate() {
+    private static RestTemplate getRestTemplate() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        if(REST_TEMPLATE == null) {
+            HttpClientBuilder httpClientBuilder = HttpClients.custom()
+                    .setMaxConnPerRoute(getMaxConnectionPerRouteFromEnv())
+                    .setMaxConnTotal(getTotalMaxConnectionsFromEnv()).disableCookieManagement();
+            RestTemplate restTemplate = null;
+            if (getSSLBypassFromEnv()) {
+                TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+                SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+                        .loadTrustMaterial(null, acceptingTrustStrategy).build();
+                SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, new HostnameVerifier() {
+                    public boolean verify(String arg0, SSLSession arg1) {
+                        return true;
+                    }
+                });
+                httpClientBuilder.setSSLSocketFactory(csf);
+            }
+            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+            requestFactory.setHttpClient(httpClientBuilder.build());
+            REST_TEMPLATE = new RestTemplate(requestFactory);
+        }
+
 		return REST_TEMPLATE;
 	}
 
-	/**
-	 * Encodes the given data string to Base64 format.
-	 *
-	 * @param data The data string to be encoded.
-	 * @return Base64 encoded string.
-	 */
-	public static String base64Encode(String data) {
-		return Base64.getEncoder().encodeToString(data.getBytes());
-	}
+
+    private static Integer getMaxConnectionPerRouteFromEnv() {
+        Integer value = System.getProperty(MAX_CONN_PER_ROUTE) != null ? Integer.parseInt(System.getProperty(MAX_CONN_PER_ROUTE)) : 20;
+        utilLogger.debug(LOGGER_SESSIONID, LOGGER_IDTYPE, "Maximum Connection per Host: ", value.toString());
+        return value;
+    }
+
+    private static Integer getTotalMaxConnectionsFromEnv() {
+        Integer value = System.getProperty(MAX_TOT_CONN) != null ? Integer.parseInt(System.getProperty(MAX_TOT_CONN)) : 100;
+        utilLogger.debug(LOGGER_SESSIONID, LOGGER_IDTYPE, "Total Maximum Connection: ", value.toString());
+        return value;
+    }
+
+    private static Boolean getSSLBypassFromEnv() {
+        Boolean value = System.getProperty(SSL_BYPASS) != null ? BooleanUtils.toBoolean(System.getProperty(SSL_BYPASS)) : sslBypass;
+        utilLogger.debug(LOGGER_SESSIONID, LOGGER_IDTYPE, "SSL Bypass Flag: ", value.toString());
+        return value;
+    }
+
+//	public static RestTemplate createRestTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
+//
+//        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+//
+//        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy)
+//                .build();
+//
+//        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+//
+//        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+//        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+//
+//        requestFactory.setHttpClient(httpClient);
+//        return new RestTemplate(requestFactory);
+//    }
 
 	/**
 	 * Flag indicating whether to log request and response details for debugging
