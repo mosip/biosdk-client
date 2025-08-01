a806d2a9450bece65baffd57f2f4b2120cbde593
Nidhi <nidhi.k@cyberpwn.com>
2025-08-01 12:07:23 +0530

    [42083] - removed IntArrayToByteArrayDeserializer & ByteArrayToIntArraySerializer from Util - biosdk (#118)
    * [42083] - removed serialize from Utils
    
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>
    
    * [42083] - added AfterburnerModule
    
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>
    
    * [42083] - removed jackson-datatype-jsr310
    
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>
    
    * [42083] - removed jackson-datatype-jsr310
    
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>
    
    * [42083] - removed loggers from biosdk
    
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>
    
    * [42083] - removed IntArrayToByteArrayDeserializer & ByteArrayToIntArraySerializer from Util - biosdk
    
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>
    
    * [42083] - fix
    
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>
    
    ---------
    
    Signed-off-by: Nidhi0201 <nidhi.k@cyberpwn.com>

--- biosdk-client/src/main/java/io/mosip/biosdk/client/utils/Util.java
+++ biosdk-client/src/main/java/io/mosip/biosdk/client/utils/Util.java
@@ -4,11 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import io.mosip.biosdk.client.config.LoggerConfig;
-import io.mosip.kernel.biometrics.entities.ByteArrayToIntArraySerializer;
-import io.mosip.kernel.biometrics.entities.IntArrayToByteArrayDeserializer;
 import io.mosip.kernel.core.exception.ExceptionUtils;
 import io.mosip.kernel.core.logger.spi.Logger;
 import org.apache.commons.lang3.BooleanUtils;
@@ -48,12 +45,8 @@ public class Util {
 
     public static ObjectMapper getObjectMapper() {
         if(mapper == null) {
-            SimpleModule module = new SimpleModule();
-            module.addSerializer(byte[].class, new ByteArrayToIntArraySerializer());
-            module.addDeserializer(byte[].class, new IntArrayToByteArrayDeserializer());
             mapper = new ObjectMapper();
             mapper.registerModule(new AfterburnerModule());
-            mapper.registerModule(module);
             mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
             mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
             mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
